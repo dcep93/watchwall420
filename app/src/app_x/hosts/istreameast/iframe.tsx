509f4f482d36dac554e727d77aac6e4a878c6ddd
Daniel Cepeda <dcep93@gmail.com>
2026-03-21 16:16:16 -0400

    x

--- app/src/app_x/hosts/istreameast/iframe.tsx
+++ app/src/app_x/hosts/istreameast/iframe.tsx
@@ -6,31 +6,38 @@ export function renderIstreameastPlayerDocument(iframeParams: IframeParams): Rea
     <html lang="en">
       <head>
         <base href={iframeParams._embedPageUrl} />
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </head>
-      <body style="margin: 0; padding: 0; overflow-y: hidden; background: #000">
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          overflowY: "hidden",
+          background: "#000",
+        }}
+      >
         <iframe
           src={iframeParams._iframeSourcePageUrl}
-          frameborder="0"
-          style="
-        overflow: hidden;
-        overflow-x: hidden;
-        overflow-y: hidden;
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-      "
+          frameBorder="0"
+          style={{
+            overflow: "hidden",
+            overflowX: "hidden",
+            overflowY: "hidden",
+            height: "100%",
+            width: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
           height="100%"
           width="100%"
-          allowfullscreen
+          allowFullScreen
           scrolling="no"
-          allowtransparency
-        ></iframe>
+          allowTransparency
+        />
       </body>
     </html>
   );
